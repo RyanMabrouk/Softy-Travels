5cc1a2b0fc663b8e8a20ae8e2710aff2bd37e4ab
Rayen Mabrouk <mabroukr911@gmail.com>
2023-11-12 18:17:52 +0100

    bug fix

--- src/services/supabase.config.js
+++ src/services/supabase.config.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { API_KEY, MAIN_URL } from "./Constants";
+import { API_KEY, MAIN_URL } from "./CONSTANTS";
 export const supabaseUrl = MAIN_URL;
 const supabaseKey = API_KEY;
 export const SUPABASE = createClient(supabaseUrl, supabaseKey);
